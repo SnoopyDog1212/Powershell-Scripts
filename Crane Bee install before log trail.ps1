b052b4f750d3c5929dd51a4311d79adeb88fd689
Jacob Decker <jacob.decker2@gmail.com>
2023-03-31 10:29:35 -0500

    Add files via upload

---
+++ Crane Bee install before log trail.ps1
@@ -0,0 +1,27 @@
+﻿## Install Script for Cranebee (currently working, just not Verbose enough)
+
+# It might be worth clearing/deleting the program data folder before installing/fixing up the uninstall script and making it reinstall
+$RealModulesFolder = Join-Path $modulesFolder -ChildPath "module"
+$Arguments = @"
+/S /netzlizenz /licpath="$($LicensePath.LocalPath)"
+"@
+$Process = Start-Process -Wait $InstallerFile -ArgumentList $Arguments -Passthru
+Write-Host "exitcode: $($Process.exitcode)"
+# I removed the module install cause it seemed to be too large of a point of failure and it would require us to manually update them 
+
+# Old Args
+#$Arguments = @"
+#/S /netzlizenz /licpath="$($LicensePath.LocalPath)" /diskinstall /offlinepath="$RealModulesFolder"
+#"@
+
+# I want it be dynamically showing the logs from C:\ProgramData\CRANIMAX\CRANEbee\Logs\CB.beeAdmin20220513 (yearmonthday)
+# This log will show you what the second installer is actually doing
+# Darren said the Start-ProcessWithLogTail function should work and that Advanced IP Scanner is a good script to look at 
+
+$ExitTime = (get-date).addseconds(1800)
+
+do{
+    $PathExists = test-path "C:\Program Files\CRANIMAX\CRANEbee\ApplicationData\CRANEbee.exe"
+    Sleep -s 1
+}
+until($PathExists -or (get-date) -gt $ExitTime)
