7e500f24d86848a3d3efa17a3b7d2132e9624e11
Chloe Decker <jacob.decker2@gmail.com>
2023-11-22 09:11:20 -0600

    Update O365 Edit Calendar Permissions.ps1
    Updated with the changes I have made overtime

--- O365 Edit Calendar Permissions.ps1
+++ O365 Edit Calendar Permissions.ps1
@@ -1,29 +1,35 @@
 ##This logs you into the O365 Server for changes
 Connect-ExchangeOnline
 
-#Setup Array's of the users you want to change
+#Who needs access:
+#for multiple users you can seperate them with commas ie ('user','user2')
 $giveAccessTo = @('user1@domain.com','user2@domain.com')
-$calendarOwners = @('user1@domain.com','user2@domain.com')
 
+#Who they need access to:
+$calendarOwners = @('user3@domain.com','user4@domain.com')
+
+#All Write-Host commands are built to display information in the console while running the script
 Write-Host "Attemping " ($giveAccessTo.Count * $calendarOwners.Count * 2) " changes"
 
 foreach($owner in $calendarOwners){
     Write-Host "Starting modifications for $owner"
+    $giveAccessTo[0]
 
+    #for Keogh, it is important to update both Calendar and Tasks permissions as they use both
     $calendar = $owner + ":\Calendar"
     $task = $owner + ":\Tasks"
 
     foreach($recievingUser in $giveAccessTo){
-        ##It may be worth making the catch block more specific incase of a misspelled email error
+        ##The try and catch blocks are designed to resolve the below errors without the script crashing out
         ##Add-MailboxFolderPermission: |Microsoft.Exchange.Management.StoreTasks.UserAlreadyExistsInPermissionEntryException|An existing permission entry was found for user: user.
         try {
-            Add-MailboxFolderPermission -Identity $calendar -User $recievingUser -AccessRights PublishingEditor
+            Add-MailboxFolderPermission -Identity $calendar -User $recievingUser -AccessRights PublishingEditor -ErrorAction Stop
         }
         catch {
-            Set-MailboxFolderPermission -Identity $calendar -User $recievingUser -AccessRights PublishingEditor
+            Set-MailboxFolderPermission -Identity $calendar -User $recievingUser -AccessRights PublishingEditor 
         }
         try {
-            Add-MailboxFolderPermission -Identity $task -User $recievingUser -AccessRights PublishingEditor
+            Add-MailboxFolderPermission -Identity $task -User $recievingUser -AccessRights PublishingEditor -ErrorAction Stop
             }
         catch {
             Set-MailboxFolderPermission -Identity $task -User $recievingUser -AccessRights PublishingEditor
@@ -34,3 +40,15 @@ foreach($owner in $calendarOwners){
 }
 
 Write-Host "All modifications complete"
+
+
+#Misc Commands I find may be useful to run individually in certain cases
+## Check permissions to Calendar
+## Get-MailboxFolderPermission -Identity user@domain.com:\Calendar 
+
+## Create list of all users permissions to Mailboxes
+#Get-Mailbox -resultsize unlimited | Get-MailboxPermission | Select Identity, User, Deny, AccessRights, IsInherited| Export-Csv -Path "c:\temp\mailboxpermissions.csv" -NoTypeInformation
+
+#Fixing hidden groups 
+##Set-UnifiedGroup -Identity "Cortana Corridor" -HiddenFromExchangeClientsEnabled:$false
+##Set-UnifiedGroup -Identity "Cortana Corridor" -HiddenFromAddressListsEnabled:$false
